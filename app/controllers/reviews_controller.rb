a71c6f6250491460236b1c3e13cf6de708800c8d
vladich <vladichnaprimer@gmail.com>
2016-11-21 21:22:57 +0200

    Create ratings

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
 
-  def index
-    @reviews = Review.all
-  end
-
-
-  def show
-  end
-
-  # GET /reviews/new
   def new
     @review = Review.new
   end
@@ -23,10 +15,10 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-
+    @review.movie_id = @movie.id
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -51,7 +43,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -60,6 +52,10 @@ class ReviewsController < ApplicationController
 
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
